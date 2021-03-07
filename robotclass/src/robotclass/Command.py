2ef578f55035f899720e8b9cecabb8f0b846e10f
JessicaGiancola <jessica.giancola@student.univaq.it>
2021-03-07 17:46:19 +0000

    first commit

---
+++ robotclass/src/robotclass/Command.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+class COMMAND:
+    CMD_MOTOR = "CMD_MOTOR"
+    CMD_LED = "CMD_LED"
+    CMD_LED_MOD = "CMD_LED_MOD"
+    CMD_SERVO = "CMD_SERVO"
+    CMD_BUZZER = "CMD_BUZZER"
+    CMD_SONIC = "CMD_SONIC"
+    CMD_LIGHT = "CMD_LIGHT"
+    CMD_POWER = "CMD_POWER" 
+    CMD_MODE ="CMD_MODE"
+    def __init__(self):
+        pass
